2f4492d928e3468cce132a9028196277f1b509d2
AnupamaKannan <anupamakannnan555@gmail.com>
2023-04-01 19:08:01 +0530

    main file added

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -3,10 +3,19 @@ import random
 
 pygame.init()
  
-WIDTH,HEIGTH=400,400
+WIDTH,HEIGTH=300,300
 
-WIN=pygame.display.set_mode(WIDTH,HEIGTH)
+window=pygame.display.set_mode((WIDTH,HEIGTH))
 pygame.display.set_caption("TIC TAC TOE")
+
+def draw_window()
+    clr=(244,244,244)
+    lineclr=(50,50,50)
+    window.fill(clr)
+    for x in range(0,3):
+        pygame.draw.line(window,(0,x*100),(width,x*100))
+        pygame.draw.line(window,(0,x*100),(width,x*100))
+    //pygame.display.update()
 
 run=True
 while run:
