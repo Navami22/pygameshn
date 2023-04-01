9eedc01ae6d8e2360ec0e2b9bdad0d6a4e3144a1
AnupamaKannan <anupamakannnan555@gmail.com>
2023-04-01 19:33:35 +0530

    main file modified

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -8,20 +8,25 @@ WIDTH,HEIGTH=300,300
 window=pygame.display.set_mode((WIDTH,HEIGTH))
 pygame.display.set_caption("TIC TAC TOE")
 
-def draw_window()
-    clr=(244,244,244)
+def draw_window():
+    clr=(255,255,255)
     lineclr=(50,50,50)
     window.fill(clr)
-    for x in range(0,3):
-        pygame.draw.line(window,(0,x*100),(width,x*100))
-        pygame.draw.line(window,(0,x*100),(width,x*100))
-    //pygame.display.update()
+    for x in range(1,3):
+        pygame.draw.line(window,lineclr,(0,x*100),(WIDTH,x*100))
+        pygame.draw.line(window,lineclr,(x*100,0),(x*100,HEIGTH))
+    pygame.display.update()
+    
+    
 
 run=True
+
 while run:
+    draw_window()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
+    
 pygame.quit()
             
 
