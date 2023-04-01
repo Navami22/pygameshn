971e9dda5071865f7b0a1aeb3c1225e4ff1e1464
AnupamaKannan <anupamakannnan555@gmail.com>
2023-04-01 20:19:34 +0530

    image added

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -7,24 +7,19 @@ WIDTH,HEIGTH=300,300
 
 window=pygame.display.set_mode((WIDTH,HEIGTH))
 pygame.display.set_caption("TIC TAC TOE")
+icon = pygame.image.load("tic-tac-toe.png")
+pygame.display.set_icon(icon)
 
 def draw_window():
     clr=(255,255,255)
     lineclr=(50,50,50)
     window.fill(clr)
-<<<<<<< HEAD
     for x in range(1,3):
         pygame.draw.line(window,lineclr,(0,x*100),(WIDTH,x*100))
         pygame.draw.line(window,lineclr,(x*100,0),(x*100,HEIGTH))
     pygame.display.update()
     
     
-=======
-    for x in range(0,3):
-        pygame.draw.line(window,lineclr,(0,x*100),(width,x*100))
-        pygame.draw.line(window,linecr,(x*100,0),(x*100,height))
-    //pygame.display.update()
->>>>>>> 32ff933255ec52d119fe025a746985c1e0a760b7
 
 run=True
 
