5178ad9b9f78236292f78d9b25a9e0aaefb0d8ff
AnupamaKannan <anupamakannnan555@gmail.com>
2023-04-01 18:54:11 +0530

    main fail added

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -6,4 +6,13 @@ pygame.init()
 WIDTH,HEIGTH=400,400
 
 WIN=pygame.display.set_mode(WIDTH,HEIGTH)
+pygame.display.set_caption("TIC TAC TOE")
+
+run=True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run=False
+pygame.quit()
+            
 
